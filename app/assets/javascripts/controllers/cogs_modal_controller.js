a20e5ea975f32d515078133872b6e20500428610
Rafał Trojanowski <rafal@prograils.com>
2014-10-04 23:24:31 +0200

    create cogs fix

--- app/assets/javascripts/controllers/cogs_modal_controller.js
+++ app/assets/javascripts/controllers/cogs_modal_controller.js
@@ -1,6 +1,6 @@
 App.CogsModalController = App.ModalController.extend({
   create: function() {
-    var cog = App.Cog.createRecord();
+    var cog = this.store.createRecord('cog');
 
     cog.on('didCreate', this, function() {
       this.close();
